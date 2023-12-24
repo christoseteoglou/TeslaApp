8658b3e17222de541a86a1e6abd36f451ec6e0e7
christoseteoglou <itsmeglue@gmail.com>
2023-12-24 18:01:39 +0100

    Initial Commit

---
+++ MyTeslaApp/MyTeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTeslaAppApp.swift
+//  MyTeslaApp
+//
+//  Created by Christos Eteoglou on 2023-12-24.
+//
+
+import SwiftUI
+
+@main
+struct MyTeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
